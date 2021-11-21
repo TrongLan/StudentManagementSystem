0cbc0c9fa967b57c8cf48445a59827df84d1d624
Trong Lan <91108170+TrongLan@users.noreply.github.com>
2021-11-21 16:13:06 +0700

    Delete WebMovie directory

--- WebMovie/src/main/java/NightOwlTV/WebMovie/Repository/WatchRepo.java
+++
@@ -1,8 +0,0 @@
-package NightOwlTV.WebMovie.Repository;
-
-import NightOwlTV.WebMovie.Entity.Watch;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-public interface WatchRepo extends JpaRepository<Watch, Long>{
-}
